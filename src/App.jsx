6ad1804756d1c707dfef69042dfd30db6bc9a292
syfaarizal <syifairgi@gmail.com>
2026-01-21 09:06:07 +0700

    add links

--- src/App.jsx
+++ src/App.jsx
@@ -60,8 +60,8 @@ const customStyles = `
 // --- CUSTOM ICONS (SOLID STYLE) ---
 
 const FaBriefcaseIcon = ({ size = 20, className }) => (
-  <svg width={size} height={size} viewBox="0 0 512 512" fill="currentColor" className={className} xmlns="http://www.w3.org/2000/svg">
-    <path d="M128 48C128 21.5 149.5 0 176 0h160c26.5 0 48 21.5 48 48v48h48c26.5 0 48 21.5 48 48v224c0 26.5-21.5 48-48 48H80c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h48V48zm48 48h160V48H176v48zM32 160v208c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16V160H32z"/>
+  <svg width={size} height={size} fill="currentColor" className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <path d="M200 48l112 0c4.4 0 8 3.6 8 8l0 40-128 0 0-40c0-4.4 3.6-8 8-8zm-56 8l0 40-80 0C28.7 96 0 124.7 0 160l0 96 512 0 0-96c0-35.3-28.7-64-64-64l-80 0 0-40c0-30.9-25.1-56-56-56L200 0c-30.9 0-56 25.1-56 56zM512 304l-192 0 0 16c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32-14.3-32-32l0-16-192 0 0 112c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-112z"/>
   </svg>
 );
 
@@ -72,8 +72,8 @@ const FaCoffeeIcon = ({ size = 20, className }) => (
 );
 
 const FaUserSecretIcon = ({ size = 20, className }) => (
-  <svg width={size} height={size} viewBox="0 0 448 512" fill="currentColor" className={className} xmlns="http://www.w3.org/2000/svg">
-    <path d="M224 16c-61.9 0-112 50.1-112 112c0 11.6 2.1 22.6 5.3 33.2C64.3 180.9 25.1 220 16.5 267.3C13.2 285.3 10.6 302.2 6.5 320c-2.3 9.9-1.9 20.3 3.6 29.3C18.6 363.3 224 496 224 496s205.4-132.7 213.9-146.7c5.6-9.1 5.9-19.4 3.6-29.3c-4.1-17.8-6.6-34.7-9.9-52.7c-8.6-47.3-47.8-86.4-100.8-106.1c3.2-10.6 5.3-21.6 5.3-33.2c0-61.9-50.1-112-112-112z"/>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor" className={className} width={size} height={size}>
+    <path d="M171-16c-36.4 0-57.8 58.3-68.3 112L72 96c-13.3 0-24 10.7-24 24s10.7 24 24 24l24 0 0 32c0 17 3.3 33.2 9.3 48l-9.3 0 0 0-20.5 0c-15.2 0-27.5 12.3-27.5 27.5 0 3 .5 5.9 1.4 8.7l28.9 86.6C40.2 379.6 16 428.1 16 482.3 16 498.7 29.3 512 45.7 512l356.6 0c16.4 0 29.7-13.3 29.7-29.7 0-54.2-24.2-102.7-62.3-135.4l28.9-86.6c.9-2.8 1.4-5.7 1.4-8.7 0-15.2-12.3-27.5-27.5-27.5l-20.5 0 0 0-9.3 0c6-14.8 9.3-31 9.3-48l0-32 24 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-30.7 0c-10.4-53.7-31.9-112-68.3-112-9.6 0-19 3.9-27.5 8.2-8.2 4.1-18.4 7.8-25.5 7.8s-17.3-3.7-25.5-7.8C190-12.1 180.6-16 171-16zm93.7 484.4l-24.8-70.9 27.9-32.5c2.7-3.2 4.2-7.2 4.2-11.4 0-9.7-7.8-17.5-17.5-17.5l-61 0c-9.7 0-17.5 7.8-17.5 17.5 0 4.2 1.5 8.2 4.2 11.4l27.9 32.5-24.8 70.9-57-180.4 35.7 0c18.4 10.2 39.5 16 62 16s43.6-5.8 62-16l35.7 0-57 180.4zM224 256c-34.7 0-64.2-22.1-75.3-53 5.7 3.2 12.3 5 19.3 5l12.4 0c16.5 0 31.1-10.6 36.3-26.2 2.3-7 12.2-7 14.5 0 5.2 15.6 19.9 26.2 36.3 26.2l12.4 0c7 0 13.6-1.8 19.3-5-11.1 30.9-40.6 53-75.3 53z"/>
   </svg>
 );
 
@@ -92,6 +92,18 @@ const BrandTikTokIcon = ({ size = 20, className }) => (
 const BrandDiscordIcon = ({ size = 20, className }) => (
   <svg width={size} height={size} viewBox="0 0 24 24" fill="currentColor" className={className} xmlns="http://www.w3.org/2000/svg">
     <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037 13.56 13.56 0 0 0-.616 1.267 18.37 18.37 0 0 0-4.733 0 13.43 13.43 0 0 0-.62-1.267.076.076 0 0 0-.078-.037 19.736 19.736 0 0 0-4.885 1.515.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028 14.09 14.09 0 0 0 1.226-1.994.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z"/>
+  </svg>
+);
+
+const BrandYoutubeIcon = ({ size = 20, className }) => (
+  <svg width={size} height={size} viewBox="0 0 24 24" fill="currentColor" className={className} xmlns="http://www.w3.org/2000/svg">
+    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+  </svg>
+);
+
+const BrandInstagramIcon = ({ size = 20, className }) => (
+  <svg width={size} height={size} viewBox="0 0 24 24" fill="currentColor" className={className} xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.266.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zM5.838 12a6.162 6.162 0 1 1 12.324 0 6.162 6.162 0 0 1-12.324 0zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm4.965-10.322a1.44 1.44 0 1 1 2.881.001 1.44 1.44 0 0 1-2.881-.001z"/>
   </svg>
 );
 
@@ -208,10 +220,10 @@ export default function App() {
         {/* --- SOCIAL ICONS --- */}
         <div className="flex gap-4 mb-8 animate-[fadeUp_0.8s_ease-out_1.2s_forwards] opacity-0 translate-y-4" style={{animationFillMode: 'forwards', animationDelay: '1.2s'}}>
           {[
-            { Icon: Instagram, href: "https://instagram.com", label: "Instagram" },
-            { Icon: BrandXIcon, href: "https://twitter.com", label: "X (Twitter)" },
-            { Icon: Youtube, href: "https://youtube.com", label: "YouTube" },
-            { Icon: BrandTikTokIcon, href: "https://tiktok.com", label: "TikTok" },
+            { Icon: BrandInstagramIcon, href: "https://instagram.com/kaishiscd", label: "Instagram" },
+            { Icon: BrandXIcon, href: "https://twitter.com/kaishiscd", label: "X (Twitter)" },
+            { Icon: BrandYoutubeIcon, href: "https://youtube.com/@kaishiscd", label: "YouTube" },
+            { Icon: BrandTikTokIcon, href: "https://tiktok.com/@kaishiscd", label: "TikTok" },
           ].map((item, idx) => (
             <a 
               key={idx}
@@ -233,6 +245,7 @@ export default function App() {
             icon={<FaBriefcaseIcon size={20} />} 
             text="My Portfolio" 
             delay="1.4s" 
+            href="https://kaishi.netlify.app/"
           />
 
           {/* AI BUTTON - MODIFIED HOVER */}
@@ -247,7 +260,7 @@ export default function App() {
           >
             <div className="flex items-center gap-4">
               <Sparkles size={20} className="text-[#ff8fa3] group-hover:text-white transition-colors" />
-              <span className="font-semibold">✨ Tanya AI tentang Kaishi</span>
+              <span className="font-semibold">Tanya AI tentang Kai Shi</span>
             </div>
             <Bot size={18} className="opacity-30 group-hover:opacity-100 group-hover:text-white transition-all" />
           </button>
@@ -255,12 +268,13 @@ export default function App() {
           <LinkButton 
             icon={<FaCoffeeIcon size={20} />} 
             text="Trakteer" 
-            delay="1.6s" 
+            delay="1.6s"
+            href="https://trakteer.id/kaishiscd" 
           />
 
           <LinkButton 
             icon={<FaUserSecretIcon size={20} />} 
-            text="Project Anonymous" 
+            text="Bisik-bisik" 
             delay="1.7s" 
           />
 
